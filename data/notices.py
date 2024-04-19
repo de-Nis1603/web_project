cad7dd5aeb86fc66fb8d05965bdae6015f152141
Den <no@>
2024-04-19 22:20:39 +0300

    Вывод всех обЪявлений на экран при запуске

---
+++ data/notices.py
@@ -0,0 +1,19 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class Notices(SqlAlchemyBase):
+    __tablename__ = 'notices'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relationship('User')
